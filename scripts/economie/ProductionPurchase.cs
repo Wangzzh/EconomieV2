a12a3990d47eefcb86bf2ae0d7fd59ab43d706db
Zizhen Wang <wangzzh96@gmail.com>
2025-11-27 16:14:50 -0800

    Circular producer test

--- scripts/economie/ProductionPurchase.cs
+++ scripts/economie/ProductionPurchase.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 public partial class ProductionPurchase(Goods goods): Purchase(goods)
 {
     public double Budget { get; set; } = 0f;
+    public double MaxPurchaseAmount = Double.MaxValue;
 
     public override double GetPurchaseAmountAtPrice(double price) {
         if (Budget <= 0f || price <= 0f) {
             return 0f;
         }
-        return Budget / price;
+        return Math.Min(Budget / price, MaxPurchaseAmount);
     }
 }
